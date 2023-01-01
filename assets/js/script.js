3eb75b01a477c0c729606d24db1526a5e3c4f222
ujuniordev <ujunior@outlook.com>
2023-01-01 22:04:44 +0000

    Add questions to js file

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// defining the questions and passing the correct answer via index
+
+const questions = [{
+        question: "When did the first World Cup edition happen?",
+        choices: ["1930", "1936", "1935", "1932"],
+
+        answerIndex: 0,
+    },
+    {
+        question: "In which country did the first World Cup happen?",
+        choices: ["Germany", "Brazil", "England", "Uruguay"],
+        answerIndex: 3,
+    },
+    {
+        question: "As of 2022, which country has the most World Cup wins?",
+        choices: ["Italy", "France", "Brazil", "Germany"],
+        answerIndex: 2,
+    },
+    {
+        question: "Which country won the first World Cup?",
+        choices: ["Italy", "Uruguay", "Germany", "Brazil"],
+        answerIndex: 1,
+    },
+    {
+        question: "In which year did England host the World Cup?",
+        choices: ["1966", "1994", "1970", "1986"],
+        answerIndex: 0,
+    },
+];
+
